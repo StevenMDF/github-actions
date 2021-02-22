d13353be536c09eeb6b5fec0a997cb65e73008c5
StevenMDF <steven.gmdf@gmail.com>
2021-02-22 16:46:14 +1300

    modified test_sample for correctness

--- test_sample.py
+++ test_sample.py
@@ -1,6 +1,8 @@
-def inc(x):
-    return x + 1
+def capitalize_string(s):
+    if not isinstance(s, str):
+        raise TypeError('Please provide a string')
+    return s.capitalize()
 
 
-def test_answer():
-    assert inc(3) == 4
+def test_capitalize_string():
+    assert capitalize_string('test') == 'Test'
